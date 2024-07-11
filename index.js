7d7e7964d5b5c7f9ab7ec6029b1de803288399bf
Neeraj912005 <pandeyneerajkakra@gmail.com>
2024-07-11 12:14:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,117 @@
+//console.log("hello")
+//let score=33
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNunber=Number(score);
+
+//console.log(typeof score);
+//"33"=>33
+//"33abc" =>NaN
+//true =>1; false=>0;
+// let isloggedIn=1
+// let booleanIsLoggedIn=Boolean(isloggedIn);
+// console.log(booleanIsLoggedIn);
+
+//let somenumber=33;
+//let StringNumber=String(somenumber);
+//console.log(StringNumber);
+//console.log(typeof StringNumber);
+
+//let value=3;
+//let negvalue = -value;
+//console.log(negvalue);
+//opretar js
+/*console.log(2*2)
+console.log(2/2)
+console.log(2+2)
+console.log(2-2)
+console.log(2**2)
+console.log(2/2)
+console.log(2-2+4)
+*/
+//some of two number add program
+/*let str1=22
+let str2=54
+let str3=str1+str2
+console.log(str3)
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log(1+"2");
+console.log("1"+2);
+console.log("23"+2*3+4-6);
+*/
+/*let gameCounter=100
+++gameCounter;
+gameCounter++;
+console.log(gameCounter)
+*/
+/*console.log(null > 0);
+console.log(null >= 0);
+console.log(null === 0);
+
+console.log(null == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+console.log(undefined === 0);
+console.log(undefined == 0);
+*/
+//primitive data type
+// 7types: string ,number,Boolearn,null,undefined,symbol,bigint
+
+
+//refrence (non primitie)
+// array objects functions
+// //js dynamic type language
+// const id='123'
+// const anotherid='123'
+// console.log(id === anotherid);
+
+
+// const heros =["harsh","mahesh","mohan"];
+// let myobj={
+//     name:"neeraj",
+//     age:22,
+// }
+// let myFunction =function(a){
+//     console.log("myFunction");
+// }
+// myFunction(1)
+// let anothername 
+// anothername = "chaiaurcode"
+
+// console.log(anothername);
+
+// let userOne ={
+//     email:"neeraj@google.com",
+//     upi:"user@ybl"
+// }
+
+// let userTwo=userOne
+// userTwo.email = "neeraj@google.com"
+// console.log(userOne.email);
+// console.log(userTwo.email);
+// console.log("hero");
+
+// let neeraj = "neerah"
+// let harry=function(){
+//     console.log(neeraj);
+// }
+// harry()
+// console.log(typeof neeraj);
+const name="hitesh"
+const repoCount =50
+console.log(name+repoCount+"value");
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+
+
+
+
+
+
+
+
+
+

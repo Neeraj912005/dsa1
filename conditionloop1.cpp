c385b658bbf3f23da5e43bfe7fd484afd8f828a4
Neeraj912005 <pandeyneerajkakra@gmail.com>
2024-09-28 12:44:30 +0530

    Add files via upload

---
+++ conditionloop1.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+int main(){
+/*int a;
+cin>>a;
+if(a>0){
+    cout<<"A is a positive number"<<endl;
+}
+else{
+cout<<" A is a negative number"<<endl;
+}*/
+//int a,b;
+//cin>>a,b;
+//cout<<"value of a and b is "<<a<<" "<<b<<endl;
+//cout<<"enter the value of a"<<endl;
+//cin>>a;
+//cout<<"enter the value of b"<<endl;
+//cin>>b;
+//if(a>b){
+//cout<<"a is greater"<<endl;
+//}
+//if(b>a){
+  //  cout<<"b is greater"<<endl;
+//}
+//int a;
+//cin>>a;
+//if(a>0){
+  //  cout<<"A is positive"<<endl;
+//}
+//else{
+//if(a<0){
+  //  cout<<"A is negative"<<endl;
+//}
+//else{
+  //  cout<<" a is zero"<<endl;
+//}
+//}
+
+//int n;
+//cin>>n;
+//int i=1;
+
+//while(i<=n){
+  //  cout<<i<<" "<<endl;
+    //i = i+1;
+///}
+//int n;
+//cin>>n;
+//int i=1;
+//int sum =0;
+
+//while(i<=n){
+  //      sum= sum +i;
+    //i = i+1;
+//}
+//cout<<"sum of value or number"<<sum<<endl;
+
+char abc;
+cin>>abc;
+if(a<=z){
+    cout<<"lower case "<<endl;
+}
+if(A<=Z){
+    cout<<"upper case"<<endl;
+}
+
+}
+
---
+++ datatype.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=12;
+    cout<<a<<endl;
+
+    char c='d';
+    cout<<c<<endl;
+     bool  b = true;
+     cout<<b<<endl;
+     float f= 1.2;
+     cout<<f<<endl;
+     double e=1.23;
+     cout<<e<<endl;
+
+     int size = sizeof(a);
+     cout<<"size of a is :"<< size <<endl;
+}
